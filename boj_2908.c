a450e9713023cf62b5f36c482512ef9d27d3817f
cho-jinyoung <54131109+cho-jinyoung@users.noreply.github.com>
2020-03-07 00:36:28 +0900

    Create boj_2908.c

---
+++ boj_2908.c
@@ -0,0 +1,36 @@
+#2020-03-06 BOJ_2908
+#숫자를 거꾸로 읽었을 때 더 큰수를 출력
+#Ex)734, 893 => 437과 398로 읽어, 437출력  
+
+
+#include <stdio.h>
+
+int main(){
+	int input_1, input_2, one, two, i, max, output[3];
+	
+	scanf("%d %d", &input_1, &input_2);
+	
+	one=input_1;
+	two=input_2;
+	
+	while(1){
+		if(one%10>two%10){
+			max=input_1;
+			break;
+		}
+		else if(one%10<two%10){
+			max=input_2;
+			break;
+		}
+		else{
+			one=one/10;
+			two=two/10;
+		}
+	}
+//	printf("%d\n", max);
+	for(i=0;i<3;i++){
+		output[i]=max%10;
+		max=max/10;
+		printf("%d", output[i]);	
+	}
+}
